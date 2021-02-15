eef906f779ab106b8c96b55f24e1e3982cb699ab
Yuxiang <cao.yux@northeastern.edu>
2021-02-15 14:59:14 -0800

    refactor: Integre ResourceManager

--- Lab3_ResourceManager/include/SDLGraphicsProgram.hpp
+++ Lab3_ResourceManager/include/SDLGraphicsProgram.hpp
@@ -14,45 +14,45 @@
 //
 //
 #if defined(LINUX) || defined(MINGW)
-    #include <SDL2/SDL.h>
-#else // This works for Mac
-    #include <SDL.h>
+#include <SDL2/SDL.h>
+#else  // This works for Mac
+#include <SDL.h>
 #endif
 
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <fstream>
 #include <stdlib.h>
 #include <time.h>
 
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
 // This class sets up a full graphics program
-class SDLGraphicsProgram{
-public:
+class SDLGraphicsProgram {
+ public:
+  // Constructor
+  SDLGraphicsProgram(int w, int h);
+  // Desctructor
+  ~SDLGraphicsProgram();
+  // Per frame update
+  void update();
+  // Renders shapes to the screen
+  void render();
+  // loop that runs forever
+  void loop();
+  // Get Pointer to Window
+  SDL_Window* getSDLWindow();
+  // Get Pointer to Renderer
+  SDL_Renderer* getSDLRenderer();
 
-    // Constructor
-    SDLGraphicsProgram(int w, int h);
-    // Desctructor
-    ~SDLGraphicsProgram();
-    // Per frame update
-    void update();
-    // Renders shapes to the screen
-    void render();
-    // loop that runs forever
-    void loop();
-    // Get Pointer to Window
-    SDL_Window* getSDLWindow();
-    // Get Pointer to Renderer
-    SDL_Renderer* getSDLRenderer();
-
-private:
-    // Screen dimension constants
-    int screenHeight;
-    int screenWidth;
-    // The window we'll be rendering to
-    SDL_Window* gWindow ;
-    // SDL Renderer
-    SDL_Renderer* gRenderer = NULL;
+ private:
+  // Screen dimension constants
+  int screenHeight;
+  int screenWidth;
+  // The window we'll be rendering to
+  SDL_Window* gWindow;
+  // SDL Renderer
+  SDL_Renderer* gRenderer = NULL;
 };
 
 #endif
