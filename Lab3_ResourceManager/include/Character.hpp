e3c46fb5dd31449325462de280f4fd06f91bdf44
Yuxiang <cao.yux@northeastern.edu>
2021-02-15 14:58:47 -0800

    refactor: Create data structures for texture and charater

--- Lab3_ResourceManager/include/Character.hpp
+++ Lab3_ResourceManager/include/Character.hpp
@@ -1,5 +1,5 @@
-#ifndef RESOURCE_MANAGER_HPP
-#define RESOURCE_MANAGER_HPP
+#ifndef CHARACTERS_HPP
+#define CHARACTERS_HPP
 
 // I recommend a map for filling in the resource manager
 #include <iterator>
@@ -7,30 +7,35 @@
 #include <memory>
 #include <string>
 
-// Try toggling this number!
-#define CHARACTERS 50
+#include "BaseTexture.hpp"
+
+namespace yx {
 
 // Just a cheap little class to demonstrate loading characters.
 class Character {
  public:
-  Character();
+  Character(const std::string& name = "Character");
 
   ~Character();
 
-  void init(int x, int y, SDL_Texture* texture);
+  void init(int x, int y, BaseTexture* texture);
 
   void update(int x, int y, int frame);
 
   void render(SDL_Renderer* ren);
-
+  std::string getName() const;
+  void setName(const std::string& newName);
  private:
   int xPos_{0}, yPos_{0};
   unsigned int currentFrame_{0};
   unsigned int LastFrame_{7};
   // An SDL Surface contains pixel data to draw an image
-  SDL_Texture* texture_ = nullptr;
+  BaseTexture* texture_ = nullptr;
   SDL_Rect src_;
   SDL_Rect dest_;
+  std::string name_;
 };
 
-#endif
+}  // namespace yx
+
+#endif  // CHARACTERS_HPP
---
+++ Lab3_ResourceManager/src/BaseTexture.cpp
@@ -0,0 +1,86 @@
+#include "BaseTexture.hpp"
+
+namespace yx {
+
+BaseTexture::BaseTexture(const std::string& name)
+    : name_(name), texture_(nullptr), width_(0), height_(0) {}
+
+BaseTexture::~BaseTexture() { free(); }
+
+bool BaseTexture::loadFromFile(std::string path, SDL_Renderer* renderer) {
+  // Get rid of preexisting texture
+  free();
+
+  // The final texture
+  SDL_Texture* newTexture = nullptr;
+
+  // Load image at specified path
+  SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+  if (loadedSurface == nullptr) {
+    SDL_Log("Unable to load image %s! SDL_image Error: %s", path.c_str(),
+            IMG_GetError());
+  } else {
+    SDL_Log("Loaded %s",path.c_str());
+    // Create texture from surface pixels
+    newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    if (newTexture == nullptr) {
+      SDL_Log("Unable to create texture from %s! SDL Error: %s", path.c_str(),
+              SDL_GetError());
+    } else {
+      // Get image dimensions
+      width_ = loadedSurface->w;
+      height_ = loadedSurface->h;
+    }
+
+    // Get rid of old loaded surface
+    SDL_FreeSurface(loadedSurface);
+  }
+
+  // Return success
+  texture_ = newTexture;
+  return texture_ != nullptr;
+}
+
+void BaseTexture::free() {
+  // Free texture if it exists
+  if (texture_ != nullptr) {
+    SDL_DestroyTexture(texture_);
+    texture_ = nullptr;
+    width_ = 0;
+    height_ = 0;
+  }
+}
+
+void BaseTexture::setColor(Uint8 red, Uint8 green, Uint8 blue) {
+  // Modulate texture rgb
+  SDL_SetTextureColorMod(texture_, red, green, blue);
+}
+
+void BaseTexture::setBlendMode(SDL_BlendMode blending) {
+  // Set blending function
+  SDL_SetTextureBlendMode(texture_, blending);
+}
+
+void BaseTexture::setAlpha(Uint8 alpha) {
+  // Modulate texture alpha
+  SDL_SetTextureAlphaMod(texture_, alpha);
+}
+
+void BaseTexture::render(int x, int y, SDL_Renderer* renderer, SDL_Rect* src,
+                         SDL_Rect* dst, double angle, SDL_Point* center,
+                         SDL_RendererFlip flip) {
+  // Set rendering space and render to screen
+  if (dst == nullptr) {
+    SDL_Rect dest = {x, y, width_, height_};
+    SDL_RenderCopyEx(renderer, texture_, src, &dest, angle, center, flip);
+  } else {
+    SDL_RenderCopyEx(renderer, texture_, src, dst, angle, center, flip);
+  }
+}
+
+void BaseTexture::setWidth(int w) { width_ = w; }
+void BaseTexture::setHeight(int h) { height_ = h; }
+int BaseTexture::getWidth() const { return width_; }
+int BaseTexture::getHeight() const { return height_; }
+
+}  // namespace yx
